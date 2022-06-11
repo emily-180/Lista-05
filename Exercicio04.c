ecbf34715a563c555d10713939627130e7a19c47
Emily Moreira Ferreira <emily.moreira.8@gmail.com>
2022-06-11 20:21:50 -0300

    Add files via upload

---
+++ Exercicio04.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int fibonacci(int quant){
+	int res, ant=1, atual=1, cont;
+	printf("%d\n", ant);
+	res= atual;
+	for(cont=1; res <= quant; cont++){	
+		printf("%d\n", res);
+		res = atual + ant ;
+		ant = atual;
+		atual = res;
+	}
+	return 0;
+}
+//----------------------------------------------------
+int main(){
+	int res, quant;
+	printf("Digite a quantidade: ");
+	scanf("%d", &quant);
+	res = fibonacci(quant);
+	printf(res);
+}
