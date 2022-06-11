851a31a172ff4bb7252968fc458f2ba8290373c9
Emily Moreira Ferreira <emily.moreira.8@gmail.com>
2022-06-11 18:15:15 -0300

    Add files via upload

---
+++ Exercicio02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int potencia(exp, base){
+	int res=1, cont;
+	for(cont=1; cont<=exp; cont++){		
+		res =  base * res;	
+	}
+	return res;
+}
+//-------------------------------------------------
+int main(){
+	int res, exp, base;
+	printf("Digite a base e o seu exponte: ");
+	scanf("%d%d", &base, &exp);
+	res = potencia(exp, base);
+	printf("A potencia de %d ^%d = %d", base, exp, res);
+}
