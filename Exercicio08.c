cf499c379a7a84fa48c749d97360bb012c1c807b
Emily Moreira Ferreira <emily.moreira.8@gmail.com>
2022-06-15 07:16:15 -0300

    Update Exercicio08.c

--- Exercicio08.c
+++ Exercicio08.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 double pesoIdeal(double alt, char sexo){
 	int resultado;
-	if(sexo && "F")
+	if(sexo == "F")
 		return resultado = 62.1 * alt - 44.7;
-	else if (sexo && "M")
+	else if (sexo == "M")
 		return resultado =  72.7 * alt - 58;	 
 }
 //----------------------------------------------------
